7e9c1ad401291da258df6618f88c2465a1979b6d
Phil Plückthun <phil@piemapping.com>
2016-07-22 11:32:24 +0100

    Add hyperterm configuration

---
+++ hyperterm.js
@@ -0,0 +1,19 @@
+module.exports = {
+  config: {
+    fontSize: 11,
+    fontFamily: 'Monoid, "Source Code Pro", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    cursorColor: '#F81CE5',
+    borderColor: '#333',
+    css: '',
+    termCSS: '',
+    padding: '12px 14px',
+  },
+
+  plugins: [
+    'hypertheme',
+    'hyperterm-draculicious'
+  ],
+
+  // `~/.hyperterm_plugins/local/`
+  localPlugins: []
+};
