1bc931a14dd633df7f4a860982012c9d25171781
Phil Pluckthun <phil@plckthn.me>
2016-07-30 20:50:48 +0100

    Remove hyperterm overlay

--- hyperterm.js
+++ hyperterm.js
@@ -13,29 +13,12 @@ module.exports = {
         line-height: 1;
       }
     `,
-    padding: '0',
-
-    overlay: {
-      animate: false,
-      alwaysOnTop: true,
-      hasShadow: false,
-      hideOnBlur: true,
-      hideDock: false,
-      hotkeys: ['Option+Space'],
-      resizable: true,
-      position: 'top',
-      primaryDisplay: false,
-      startup: true,
-      size: 1,
-      tray: false,
-      unique: true
-    }
+    padding: '0'
   },
 
   plugins: [
     'hypertheme',
     'hyperterm-draculicious',
-    'hyperterm-overlay',
     'hyperterm-final-say'
   ],
 
