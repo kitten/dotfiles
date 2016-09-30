63e2e264d951bbc0acbd64f6f5ee437cb765afe9
Phil Plückthun <phil@piemapping.com>
2016-09-30 15:32:37 +0100

    Update hyperterm configuration

--- hyperterm.js
+++ hyperterm.js
@@ -2,38 +2,20 @@ module.exports = {
   config: {
     fontSize: 12,
     fontFamily: '"Fira Code", Monoid, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-    cursorColor: 'rgba(189, 195, 199, .8)',
     css: `
       header {
         box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.17);
       }
     `,
-    padding: '0',
-
-    overlay: {
-      animate: false,
-      alwaysOnTop: true,
-      hashShadow: false,
-      hideOnBlur: false,
-      hideDock: false,
-      hotkeys: ['CommandOrControl+Z'],
-      resizable: false,
-      position: 'top',
-      primaryDisplay: false,
-      startup: true,
-      size: 1,
-      tray: false,
-      unique: true
-    }
+    padding: '0'
   },
 
   plugins: [
-    'hyperterm-draculicious',
+    'hyperterm-adventurous',
     'hyperterm-title',
-    'hyperterm-overlay',
     'hyperterm-alternatescroll',
     'hyperterm-focus-reporting',
-    'hyperterm-paste',
+    'hyperterm-cursor',
     'hyperterm-final-say'
   ],
 
