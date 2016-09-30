273ea0e782c4fe1227c04df346c6aa114fd906c6
Phil Plückthun <phil@piemapping.com>
2016-09-30 15:41:05 +0100

    Remove border color from hyperterm

--- hyperterm.js
+++ hyperterm.js
@@ -6,6 +6,10 @@ module.exports = {
       header {
         box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.17);
       }
+
+      .hyperterm_main {
+        border-color: transparent !important;
+      }
     `,
     padding: '0'
   },
