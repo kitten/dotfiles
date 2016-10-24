0bf142fc2156b0534c5ec413197e6ab56a592e94
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2016-10-24 01:15:34 +0100

    Add new TsuImport and .hyper file

--- hyper.js
+++ hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
-    fontSize: 12,
-    fontFamily: '"Fira Code", Monoid, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontSize: 13,
+    fontFamily: '"Operator Mono", "Fira Code", Monoid, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     css: `
       header {
         box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.17);
@@ -11,17 +11,17 @@ module.exports = {
         border-color: transparent !important;
       }
     `,
+    termCSS: `
+      x-row > span {
+      }
+    `,
     padding: '0'
   },
 
   plugins: [
     'hyperterm-adventurous',
     'hyperterm-title',
-    // 'hyperterm-alternatescroll',
     'hyperterm-focus-reporting',
-    // 'hyperterm-cursor',
-    'hyperterm-tabs',
-    'hyperterm-final-say'
   ],
 
   // `~/.hyperterm_plugins/local/`
