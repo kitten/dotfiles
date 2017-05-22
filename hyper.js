c353569640554831c88894238151e7331f2c381e
Phil Plückthun <phil@plckthn.me>
2017-05-22 15:41:51 +0100

    Fix tmux and add colour scheme for hyper

--- hyper.js
+++ hyper.js
@@ -1,13 +1,42 @@
+const backgroundColor = '#191B1F'
+const foregroundColor = '#fcfefb'
+const cursorColor = 'rgba(0, 102, 255, 0.8)'
+const borderColor = '#404449'
+const colors = {
+  black: backgroundColor,
+  red: '#D3422E', // red
+  green: '#4BAE16', // green
+  yellow: '#F5BB12', // yellow
+  blue: '#0066ff', // blue
+  magenta: '#de347a', // pink
+  cyan: '#8cfff8', // cyan
+  white: '#BFD7DB', // light gray
+  lightBlack: '#97a0a0', // medium gray
+  lightRed: '#f25a55', // red
+  lightGreen: '#a2e185', // green
+  lightYellow: '#FFC620', // yellow
+  lightBlue: '#00a0e2', // blue
+  lightMagenta: '#fc6aca', // pink
+  lightCyan: '#7fd6fa', // cyan
+  colorCubes: '#fcfefb', // white
+  grayscale: foregroundColor
+}
+
 module.exports = {
   config: {
-    fontSize: 13,
+    foregroundColor,
+    backgroundColor,
+    borderColor,
+    cursorColor,
+    colors,
+    fontSize: 12,
     fontFamily: '"Operator Mono", monospace',
     css: `
       header {
         box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.17);
       }
 
-      .hyperterm_main {
+      .hyper_main {
         border-color: transparent !important;
       }
     `,
@@ -16,12 +45,10 @@ module.exports = {
         font-weight: 500;
       }
     `,
-    padding: '0'
+    padding: 0
   },
 
   plugins: [
-    'hyperterm-adventurous',
-    'hyperterm-title',
     'hyperterm-focus-reporting'
   ],
 
